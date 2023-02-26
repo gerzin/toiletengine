09a574ff16745e8c37a2e0a3282b736e8adf55ea
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2023-02-26 02:11:20 +0100

    Improve EXPECT_NE

--- src/uci/uci_test.cpp
+++ src/uci/uci_test.cpp
@@ -10,7 +10,7 @@ TEST(UciTest, UciCommand)
     ostringstream uci_output;
     uci::start(uci_command, uci_output);
     auto uci_output_str = uci_output.str();
-    EXPECT_NE(uci_output_str.find("id"), std::string::npos);
-    EXPECT_NE(uci_output_str.find("author"), std::string::npos);
+    EXPECT_NE(uci_output_str.find("id name toiletengine"), std::string::npos);
+    EXPECT_NE(uci_output_str.find("id author"), std::string::npos);
     EXPECT_NE(uci_output_str.find("uciok"), std::string::npos);
 }
